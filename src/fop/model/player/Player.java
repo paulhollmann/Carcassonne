4818d4abaceae758d4da663a9e1c05e91084d285
Robin <Robin@Robin>
2020-03-23 08:54:46 +0100

    AI Player Zwischenstand

--- src/fop/model/player/Player.java
+++ src/fop/model/player/Player.java
@@ -1,7 +1,10 @@
 package fop.model.player;
 
+import java.util.Random;
+
 import fop.controller.GameController;
 import fop.model.gameplay.GamePlay;
+import fop.model.gameplay.Gameboard;
 import fop.model.interfaces.GameConstants;
 import fop.model.interfaces.PlayerMethods;
 import fop.model.tile.Position;
@@ -59,12 +62,51 @@ public class Player implements PlayerMethods {
 	public void draw(GamePlay gp, Tile tile) {
 		GameController gc = gp.getGameController();
 		// TODO
+		
+		Tile[/*x*/][/*y*/] board = gc.getGameBoard().getBoard();
+		
+		//x direction
+		outter:
+		for(int x=0;x<board.length;x++) {
+			//y direction
+			for(int y=0;y<board.length;y++) {
+				if(gc.getGameBoard().isTileAllowed(tile, x, y)) {
+					gc.getGameBoard().newTile(tile, x, y);
+					break outter;
+				}
+				else {
+					//Do nothing
+				}
+			}
+		}
 	}
 
 	public void placeMeeple(GamePlay gp) {
 		GameController gc = gp.getGameController();
 		// TODO
 		// if no position is allowed, you have to call nextRound() by yourself.
+		if(gc.getGameBoard().getMeepleSpots() == null) {
+			gp.nextRound();
+		}
+		else {
+			//gc.getGameBoard().getMeepleSpots();
+			boolean[] meepleSpots = gc.getGameBoard().getMeepleSpots().clone();
+			
+			int trueSpots = 0;
+			
+			for(int i =0;i<gc.getGameBoard().getMeepleSpots().length;i++) {
+				if(meepleSpots[i]) {
+					trueSpots += 1;
+				}
+				else {
+					//Do nothing
+				}
+			}
+			
+			Random random = new Random();
+			int spot = random.nextInt(trueSpots);
+			//gp.placeMeeple(spot);
+		}
 		// to place a meeple, call gp.placeMeeple(...).
 	}
 
